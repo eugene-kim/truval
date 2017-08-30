029bf9b80a609678e94701908379bdecfa0c8ffa
eugene-kim <eugene.kim@salesforce.com>
2017-08-29 23:08:30 -0700

    Revert .gitignore

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGNodeList.d
+++ ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGNodeList.d
@@ -1,6 +1,3 @@
 dependencies: \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
+  /Users/eugene.kim/Projects/personal/focus/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
+  /Users/eugene.kim/Projects/personal/focus/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h
--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/Yoga.d
+++ ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/Yoga.d
@@ -1,6 +1,2 @@
 dependencies: \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
-  /Users/eugene.kim/Projects/personal/focus-app/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
+  /Users/eugene.kim/Projects/personal/focus/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c
