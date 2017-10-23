abd9d2dc40e46ea59d4d5766cf8915188f15c157
eugene-kim <eugene.kim@salesforce.com>
2017-10-23 12:55:38 -0700

    Modularize schema and resolvers

---
+++ server/graphql/types/CategoryType.js
@@ -0,0 +1,38 @@
+const categoryTypeDef = `
+  type Category {
+    id: ID!
+    name: String!
+    color: String!
+    isPrimary: Boolean!
+    user: User!
+  }
+
+  type Query {
+    categories(userId: ID!): [Category]
+    category(id: ID!): Category
+  }
+
+  type Mutation {
+    createCategory(
+      name: String!,
+      color: String!,
+      userId: ID!,
+
+      # Optional
+      isPrimary: Boolean,
+    ): Category
+
+    # You shouldn't be able to move a category from one User to another,
+    # so userId is not included in the updateCategory mutation.
+    updateCategory(
+      id: ID!,
+      name: String,
+      color: String,
+      isPrimary: Boolean,
+    ): Category
+    deleteCategory(id: ID!): String!
+  }
+`;
+
+
+module.exports = categoryTypeDef;
