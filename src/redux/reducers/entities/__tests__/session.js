a74eabc3890849bc1e485a8ecbdac254674a1feb
Eugene Kim <eugene.kim@c02v31elg8wl.internal.salesforce.com>
2017-11-29 19:20:11 -0800

    Add immutable.JS

---
+++ src/redux/reducers/entities/__tests__/session.js
@@ -0,0 +1,10 @@
+describe('Session entity reducer', () => {
+  describe('action: ADD_SESSION', () => {
+  });
+
+  describe('action: EDIT_SESSION', () => {
+  });
+
+  describe('action: DELETE_SESSION', () => {
+  });
+});
---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,22 @@
+import { Map } from 'immutable';
+
+
+export default {
+  entities: {
+    user: {
+      entities: Map(),
+    },
+
+    session: {
+      entities: Map(),
+    },
+
+    activity: {
+      entities: Map(),
+    },
+
+    category: {
+      entities: Map(),
+    },
+  },
+};
