57d9ec5ecc3cf92b3dc1a38e366e11a8df49a1c0
Eugene Kim <eugenekim.3.21@gmail.com>
2018-02-11 13:13:22 -0800

    Move refactored normalizatin logic into reduxify directory

---
+++ src/graphql/client/reduxify/makeReduxFriendly.js
@@ -0,0 +1,64 @@
+import _ from 'lodash';
+import pluralize from 'pluralize';
+
+// Utility
+import {renameKey} from 'libs/util/objectUtil';
+import astReader from '../astReader';
+import {getReduxEntityName} from '../reduxify';
+
+
+/**
+ * normalizeGql() doesn't make any assumptions about the query and the data coming in.
+ * This is good in that it's not prescriptive, but the normalized data then needs to
+ * be adjusted so that it can be put into our Redux store.
+ * Entity names such as `createUser` need to be converted to its singular entity type name.
+ * In this case, `createUser` will become `user`. Should a user entity already exist,
+ * we'll merge the two objects.`
+ */
+const makeReduxFriendly = (normalizedGqlResponse, operationAST, schemaDoc) => {
+
+  // TODO: If the normalized response is too big, we might want to use an immutable library
+  // like Immutable.js to increase performance.
+  const reduxFriendlyData = _.cloneDeep(normalizedGqlResponse);
+  const {entities} = reduxFriendlyData;
+
+  // Rename all root field names into Redux friendly names.
+  const operationName = astReader.getOperationName(operationAST);
+  const rootFieldNames = astReader.getOperationRootFieldNames(operationAST);
+  const rootFieldNameTypes = rootFieldNames.map(rootFieldName => ({
+    name: rootFieldName,
+    type: astReader.getOperationFieldType(operationName, rootFieldName, schemaDoc),
+  }));
+
+  rootFieldNameTypes.map(rootFieldNameType => {
+    const {name, type} = rootFieldNameType;
+    const reduxEntityName = getReduxEntityName(type);
+
+    renameKey(entities, name, reduxEntityName);
+  });
+
+  // Make all plural entity keys singular.
+  // If both singular and plural keys exist, merge plural into singular.
+  Object.keys(entities).map(key => {
+    if (pluralize.isPlural(key)) {
+      const reduxEntityName = getReduxEntityName(key);
+
+      // plural and redux entity name exist. Merge the two together and delete plural key.
+      if (entities.hasOwnProperty(reduxEntityName)) {
+        const reduxEntities = entities[reduxEntityName];
+        const pluralEntities = entities[key];
+
+        entities[reduxEntityName] = _.merge({}, pluralEntities, reduxEntities);
+
+        delete entities[key];
+      } else { // Only plural key exists. Simply rename the property on the object.
+        renameKey(entities, key, reduxEntityName);
+      }
+    }
+  });
+
+  return reduxFriendlyData;
+}
+
+
+export default makeReduxFriendly;
---
+++ src/graphql/client/reduxify/normalizeGql.js
@@ -0,0 +1,161 @@
+import {schema} from 'normalizr';
+import {visit} from 'graphql/language/visitor';
+import _ from 'lodash';
+import astReader, {GQL_FIELD_TYPES} from '../astReader';
+
+const NORMALIZR_SCHEMA_TYPES = {
+  ARRAY: 'ArraySchema',
+  VALUES: 'ValuesSchema',
+  UNION: 'UnionSchema',
+  OBJECT: 'ObjectSchema',
+  ENTITY: 'EntitySchema',
+};
+
+
+const normalizeGql = (operationAST, schemaDoc) => {
+  const normalizrSchema = {};
+  const stack = [];
+  let operationSchema;
+  let operationName;
+
+  /**
+   * A visitor is an object that's passed into GraphQL's visit function. The visitor contains methods
+   * that are run as nodes in the abstract syntax tree are traversed. We'll take advantage of visit()
+   * that GraphQL provides by creating a normalizr schema that will eventually mirror the structure of
+   * operation in the abstract syntax tree as we traverse over the nodes.
+   * 
+   * More information on visit() and how to create a visitor can be found here:
+   * https://github.com/graphql/graphql-js/blob/master/src/language/visitor.js
+   */
+  const visitor = {
+    Document(node) {
+      if (node.definitions.length > 1) {
+        console.error('Multiple operations not supported at the moment.');
+      }
+    },
+    OperationDefinition: {
+      enter(node) {
+        const {operation} = node;
+        const operationKey = `${operation}Type`;
+        
+        operationName = schemaDoc[operationKey].name;
+
+        if (!operationName) {
+          throw `${operationKey} is not a valid operation!`;
+        }
+
+        operationSchema = schemaDoc.types.find(type => type.name === operationName);
+      },
+    },
+    Field: {
+      enter(node) {
+        // Skip over scalar fields - normalizr automatically adds those properties in.
+        if (astReader.isScalarNode(node)) {
+          return false;
+        }
+
+        if (!astReader.entityContainsId(node)) {
+          throw `Every non scalar field must include the id field in order for normalizr to work properly.`;
+        }
+
+        const fieldName = astReader.getFieldName(node);
+
+        // An operation root field is a field that directly follows an operation name (query, mutation, subscription).
+        const operationRootField = astReader.getOpRootField(node, operationSchema);
+        const parent = astReader.getCurrentParent(stack);
+
+        // An operation root field:
+        // (1) will match one of field names on the operation type in the schema and
+        // (2) has no parents on the stack (it's the first field you come across when parsing a query).
+        // 
+        // A field that matches one of the root fields on the operation type but has
+        // a parent is an operation child field with simply the same name as an operation root field.
+        const isOperationRootField = operationRootField && !parent;
+
+        if (isOperationRootField) {
+
+          // There's a good chance that the field name on this root field is not something we
+          // can use in the Redux store, e.g. fieldName is something like `createUser`.
+          // This is okay. This method is simply for normalizing the gql response, so we have
+          // to keep this name for now, with any assumptions.
+          // Adjustments will be made after the normalizr schema has been created.
+          const nodeNormalizrSchema = createNormalizrSchema(fieldName, operationRootField.type);
+          const fieldType = astReader.getNodeFieldType(operationRootField.type);
+
+          Object.assign(normalizrSchema, {[fieldName]: nodeNormalizrSchema});
+          stack.push({fieldType, nodeNormalizrSchema})
+        } else { /* This is a descendant field on an operation. */
+          
+          // Grab the parent from the stack so we can grab the current gql node type and
+          // assign this node's normalizr schema to the parent's normalizr schema.
+          const parentNormalizrSchema = parent.nodeNormalizrSchema;
+          const parentFieldType = parent.fieldType;
+          const parentSingleSchema = getNonPolymorphicSchema(parentNormalizrSchema);
+          const parentNormalizrSchemaDefinition = parentSingleSchema.schema;
+
+          // Create normalizr schema for current node.
+          const fieldTypeObject = astReader.getChildFieldType(fieldName, parentFieldType, schemaDoc);
+          const fieldTypeName = astReader.getNodeFieldType(fieldTypeObject);
+          const nodeNormalizrSchema = createNormalizrSchema(fieldName, fieldTypeObject);
+
+          Object.assign(parentNormalizrSchemaDefinition, {[fieldName]: nodeNormalizrSchema});
+          parentSingleSchema.define(parentNormalizrSchemaDefinition);
+
+          stack.push({fieldType: fieldTypeName, nodeNormalizrSchema});
+        }
+      },
+
+      leave() {
+        const stackEntry = stack.pop();
+      },
+    },
+  };
+
+  visit(operationAST, visitor);
+
+  // The GraphQL responses always contain the `data` root property,
+  // so we'll have to account for this to normalize data properly.
+  const finalNormalizrSchema = {data: normalizrSchema};
+
+  return finalNormalizrSchema;
+}
+
+/**
+ * This returns the single schema definition if the passed in normalizr schema
+ * is a polymorphic one. If the normalizr schema is already a non polymorphic schema,
+ * it's simply returned.
+ */
+const getNonPolymorphicSchema = normalizrSchema => {
+  const normalizrSchemaType = normalizrSchema.constructor.name;
+
+  switch(normalizrSchemaType) {
+    case NORMALIZR_SCHEMA_TYPES.OBJECT:
+    case NORMALIZR_SCHEMA_TYPES.ENTITY:
+      return normalizrSchema;
+    case NORMALIZR_SCHEMA_TYPES.ARRAY:
+    case NORMALIZR_SCHEMA_TYPES.VALUES:
+    case NORMALIZR_SCHEMA_TYPES.UNION:
+      return normalizrSchema.schema;
+    default:
+      throw `Unknown normalizr schema type: ${normalizrSchemaType}.`;
+  }
+}
+
+const createNormalizrSchema = (fieldName, {kind, name, ofType}) => {
+  switch(kind) {
+    case GQL_FIELD_TYPES.NON_NULL:
+      return createNormalizrSchema(fieldName, ofType);
+    case GQL_FIELD_TYPES.LIST: {
+      const instanceSchema = new schema.Entity(fieldName);
+
+      return new schema.Array(instanceSchema);
+    }
+    case GQL_FIELD_TYPES.OBJECT:
+      return new schema.Entity(fieldName);
+    default:
+      throw `Unknown field kind: ${kind}. Unable to create normalizr schema.`;
+  }
+}
+
+
+export default normalizeGql;
