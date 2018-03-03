a98ac35dd75b8119c09f8357296cb49839264007
Eugene Kim <eugenekim.3.21@gmail.com>
2018-03-03 12:11:27 -0800

    Create utility file for parsing through responses

---
+++ src/redux/actions/responseUtil.js
@@ -0,0 +1,13 @@
+/**
+ * Methods used to parse responses from network calls.
+ */
+
+
+// Sometimes we expect a single entity but from data that's been normalized.
+// This lets us retrieve the lone entity.
+export  const getSingleNormalizedEntity = (entityName, normalizedResponse) => {
+  const entities = normalizedResponse.entities[entityName];
+  const entityId = Object.keys(entities)[0];
+
+  return entities[entityId];
+}
---
+++ src/redux/reducers/entities/activityTypeEntitiesReducer.js
@@ -0,0 +1,38 @@
+import {
+  addEntity,
+  updateEntity,
+  deleteEntity,
+  hydrateEntities,
+} from '../commonReducers/entityReducers';
+
+import {
+  ADD_ACTIVITY_TYPE,
+  UPDATE_ACTIVITY_TYPE_SUCCESS,
+  DELETE_ACTIVITY_TYPE,
+  UPDATE_FROM_SERVER,
+} from 'redux/actions/types';
+
+
+const activityTypeEntitiesReducer = (activityTypeEntities, action) => {
+  const {type} = action;
+
+  switch(type) {
+    case ADD_ACTIVITY_TYPE: {
+      return addEntity(action, 'activityType')(activityTypeEntities);
+    }
+    case UPDATE_ACTIVITY_TYPE_SUCCESS: {
+      return updateEntity(action)(activityTypeEntities);
+    }
+    case DELETE_ACTIVITY_TYPE: {
+      return deleteEntity(action)(activityTypeEntities);
+    }
+    case UPDATE_FROM_SERVER: {
+      return hydrateEntities(entities, action, 'activityType');
+    },
+    default:
+      return activityTypeEntities;
+  }
+}
+
+
+export default activityTypeEntitiesReducer;
