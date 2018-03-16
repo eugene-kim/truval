78bcc60e5cab3f2da4f8423102366200a63f0a89
Eugene Kim <eugenekim.3.21@gmail.com>
2018-03-16 13:39:12 -0700

    Organize selectors into its own directory, split into various files for each part of state

---
+++ src/redux/reducers/selectors/appSelectors.js
@@ -0,0 +1,2 @@
+export const getUserProps = state => state.app.user.props;
+export const getUserFetchStatus = state => state.app.user.fetchStatus;
---
+++ src/redux/reducers/selectors/entitySelectors.js
@@ -0,0 +1,37 @@
+import invariant from 'invariant';
+
+export const getSessionEntities = state => getEntities({entityType: 'session', state});
+export const getSessionFetchStatus = state => getEntityFetchStatuses({entityType: 'session', state});
+export const getNewSessionFetchStatus = state => getNewEntityFetchStatus({entityType: 'session', state});
+
+export const getCategoryEntities = state => getEntities({entityType: 'category', state});
+export const getCategoryFetchStatus = state => getEntityFetchStatuses({entityType: 'category', state});
+export const getNewCategoryFetchStatus = state => getNewEntityFetchStatus({entityType: 'category', state});
+
+export const getActivityTypeEntities = state => getEntities({entityType: 'activityType', state});
+export const getActivityTypeFetchStatus = state => getEntityFetchStatuses({entityType: 'activityType', state});
+export const getNewActivityTypeFetchStatus = state => getNewEntityFetchStatus({entityType: 'activityType', state});
+
+export const getActivityInstanceEntities = state => getEntities({entityType: 'activityInstance', state});
+export const getActivityInstanceFetchStatus = state => getEntityFetchStatuses({entityType: 'activityInstance', state});
+export const getNewActivityInstanceFetchStatus = state => getNewEntityFetchStatus({entityType: 'activityInstance', state});
+
+export const getEntities = ({entityType, state}) => state.entities[entityType].entities;
+
+export const getEntityFetchStatuses = ({entityType, state}) => state.entities[entityType].fetchStatus;
+export const getEntityFetchStatus = ({id, entityType, state}) => getEntityFetchStatuses({entityType, state})[id];
+export const getNewEntityFetchStatus = ({entityType, state}) => state.entities[entityType].new.fetchStatus;
+
+export const getEntityByName = ({name, entityType, state}) => {
+  invariant(
+    name && entityType && state,
+    `Parameters 'name', 'entityType', and 'state' must be provided!`
+  );
+
+  const entities = getEntities({entityType, state});
+  const entityId = Object.keys(entities).find(id => entities[id].name === name);
+
+  return entities[entityId];
+}
+
+export const getEntityById = ({id, entityType, state}) => getEntities({entityType, state})[id];
