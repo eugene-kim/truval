73ab70a1618976f23d0250e588d250f5fc3766e6
Eugene Kim <eugenekim.3.21@gmail.com>
2018-05-19 19:03:55 -0700

    Create shadows style file

---
+++ src/view/styles/shadows.js
@@ -0,0 +1,15 @@
+export default {
+  buttonShadow_1: `
+    shadow-opacity: 0.10
+    shadow-radius: 4px
+    shadow-color: #32325D
+    shadow-offset: 0px 2px
+  `,
+
+  buttonShadow_2: `
+    shadow-opacity: 0.15
+    shadow-radius: 1px
+    shadow-color: #000000
+    shadow-offset: 0px 1px
+  `,
+}
